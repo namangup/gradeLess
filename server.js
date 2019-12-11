6a9bf0d0e8b126b6ea9623aee2083595df0c32fe
Naman Gupta <32638824+namangup@users.noreply.github.com>
2019-12-12 01:07:54 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,225 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors')
+
+const multer = require('multer');
+
+var index  = 1;
+var index2 = 1;
+var file_to_be_proc
+
+
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'upload');
+  },
+  filename: function (req, file, cb) {
+  	file_to_be_proc = 'sheet' + Date.now()  + '.' + file.fieldname;
+    cb(null,  file_to_be_proc );
+    index= index + 1;
+  }
+})
+var storageb = multer.diskStorage({
+  destination: function(req, files, cb){
+    cb(null, 'upload');
+  },
+  filename: function (req, files, cb){
+    file_to_be_proc = 'sheet' + Date.now()  + '.' + file.fieldname;
+    cb(null,  file_to_be_proc );
+  }
+})
+app.use(express.static('images'));
+var upload = multer({ storage: storage })
+
+
+var uploadb = multer({ storage:storageb })
+
+var bodyParser = require('body-parser');
+
+var fs = require('fs');
+var util = require('util');
+var corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({
+	extended:true
+}));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/upload', (req, res) => res.send('Access Restricted: Contact Sarthak Rout'));
+app.listen(port, () => console.log(`Express server listening on port ${port}!`));
+
+
+
+var logFile = fs.createWriteStream('response.txt', { flags: 'a' });
+  // Or 'w' to truncate the file every time the process starts.
+var logStdout = process.stdout;
+
+var ocrFile = fs.createWriteStream('coord.txt', {flags: 'a'});
+
+var ansFile = fs.createWriteStream('answerkey.txt', {flags: 'a'});
+
+console.log = function () {
+  
+  logStdout.write(util.format.apply(null, arguments) + '\n');
+}
+write1 = function(){
+  logFile.write(util.format.apply(null, arguments) + ' ');
+}
+write11 = function(){
+  ocrFile.write(util.format.apply(null, arguments) + ' ');
+}
+write2 = function(){
+	logFile.write('\n');
+}
+write21 = function(){
+  ocrFile.write('\n');
+}
+writeans = function(){
+  ansFile.write(util.format.apply(null, arguments) + ' ');
+}
+app.post('/', upload.none(),  (req, res) => {
+  const formData = req.body;
+  write1('(');
+  write1('roll:',formData.roll);
+  write1('xref11: ', formData.xref11);
+  write1('yref11: ', formData.yref11);
+  write1('xref21: ', formData.xref21);
+  write1('yref21: ', formData.yref21);
+  write1('ans: ', formData.ans);
+  write1('qtype: ', formData.qtype);
+  write1(')');
+  write2();
+
+  write11(formData.page);
+  write11(((formData.xref11*1653)/700 + 10).toFixed(0));
+  write11(((formData.yref11*2337)/990 + 10).toFixed(0));
+  write11(((formData.xref21*1653)/700 - 10).toFixed(0));
+  write11(((formData.yref21*2337)/990 - 10).toFixed(0));
+  write11(formData.qtype);
+  write21();
+
+  writeans(formData.ans);
+
+  console.log('succesful post');
+  res.send({
+  	status: true,
+  });
+
+});
+
+var imgfilename="null";
+var pdflength = 0;
+
+function openFile(req, res){
+
+	const { spawn } = require("child_process");
+
+var file_to_be_opened = 'upload/' + file_to_be_proc;
+var str =index2 + ''
+
+console.log("python has started working");
+
+var pyProcess = spawn("python", ["pdf2img.py",file_to_be_opened, str]);
+
+index2 = index2 + 10;
+
+
+
+pyProcess.stdout.setEncoding("utf8");
+
+pyProcess.stdout.on("data", data => {
+
+  var img = data.toString();
+
+  var imgAr = img.split('\n');
+
+  pdflength = imgAr.length - 2;
+
+  imgfilename = imgAr[imgAr.length-2]
+
+  console.log(imgfilename);  console.log(img);
+
+  res.send({
+  	status: true,
+
+    length: imgAr.length-2,
+
+    imgArr: imgAr,
+
+  });
+
+});
+
+console.log("python has stopped");
+}
+
+function startML(req, res){
+  const { spawn } = require("child_process");
+
+  var imgindex = 0;
+  if(imgfilename=="null"){
+    res.send({
+
+      status:false,
+
+      message: "No file has been uploaded",
+
+    });
+  }
+
+    var imgpath = "images/" + imgfilename.substring(0, imgfilename.length -1 );
+
+    console.log(imgpath);
+
+    var pyProcess = spawn("python", ["ocr2.py",imgpath]);
+    pyProcess.stdout.setEncoding("utf8");
+
+    pyProcess.stdout.on("data", data => {
+      console.log(data);
+
+      ansstr = data.toString();
+
+      ansAr = ansstr.split('\n');
+
+      res.send({
+        AnsAr:ansAr,
+        length: ansAr.length,
+      });
+      console.log("ML Complete");
+    });
+  console.log("reached here");
+}
+
+
+app.post('/upload', upload.single('pdf'), (req, res) =>{
+console.log('upload successful');
+res.send({
+  	status: true,
+  	message: 'successful upload',
+  });
+});
+function startapp(req, res){
+  const { spawn } = require("child_process");
+  var pyProcess = spawn("python", ["clean.py"]);
+  res.send({
+    status:true,
+    msg:"cleaned" ,
+  });
+}
+app.post('/uploadmultiple', upload.array('pdf'), (req, res)=>{
+  console.log("bulk upload successful");
+  res.send({
+    status:true,
+    msg:"bulk upload successful",
+  });
+});
+app.get('/process', openFile);
+app.get('/startml', startML);
+app.get('/startapp', startapp);
